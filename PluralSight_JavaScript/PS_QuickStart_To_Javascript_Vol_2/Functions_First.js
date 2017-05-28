1f19b80ffe1a38015d7b8dbeb8fa3b7f9851bcc8
dp_chua <dp_chua@hotmail.com>
2017-05-28 02:13:14 -0400

    while loops and functions

---
+++ PluralSight_JavaScript/PS_QuickStart_To_Javascript_Vol_2/Functions_First.js
@@ -0,0 +1,20 @@
+// var name = "Mark";
+// console.log("Oh hi " + name);
+//
+// var name2 = "Lisa";
+// console.log("Oh hi " + name2);
+
+//
+// Function -- allows to associate functions with a name to command
+
+//  alert is a command.
+// alert("Some text!");
+
+// console.log is also a function (it is a command that has a name and it does something to the information that we're passing through it)
+function sayHello(name) {
+  console.log("Oh hi " + name);
+}
+
+sayHello("Mark");
+sayHello("Daniel");
+sayHello("Denny");
