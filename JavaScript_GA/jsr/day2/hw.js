b68de83d103e5ca09568661329a5d1004afd9dc9
david-chua <dp_chua@hotmail.com>
2018-01-16 19:09:13 -0500

    hw2

---
+++ JavaScript_GA/jsr/day2/hw.js
@@ -0,0 +1,12 @@
+var x = 5;
+var y = 6;
+var z = 7;
+
+var p = (x + y + z) / 2
+
+
+// Area
+// Area = Sqrt(p (p-x)8(p-y)8(p-z))
+Area = Math.sqrt(p*(p-x)*(p-y)*(p-z));
+
+console.log(Area)
---
+++ JavaScript_GA/jsr/day2/index.js
@@ -0,0 +1,10 @@
+var x = 1;
+var y = 2;
+
+if(x > y) {
+  console.log("x is greater y");
+} else {
+  console.log("y is greater than x")
+}
+
+console.log(Math.sqrt(4));
